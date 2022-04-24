5a37dc4a0066d6ffa7b405b7c17bc5044bff55d2
GuidoMiguez <grmiguez@gmail.com>
2022-04-24 15:53:02 -0300

    Añado sala 4 con esferas, queda pendiente modificar el movimiento

---
+++ TGC.MonoGame.TP/Elements/MovingSphere.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TGC.MonoGame.TP.Geometries;
+
+namespace TGC.MonoGame.TP.Elements
+{
+    public class MovingSphere : MovingObject
+    {
+        public SpherePrimitive Sphere { get; set; }
+        public MovingSphere(Vector3 StartPosition, Vector3 EndPosition, GraphicsDevice graphicsDevice, float speed = 0.8f) : base(StartPosition, EndPosition, graphicsDevice, speed)
+        {
+            Sphere = new SpherePrimitive(graphicsDevice, 1, 16, Color.Red);
+        }
+    }
+}
