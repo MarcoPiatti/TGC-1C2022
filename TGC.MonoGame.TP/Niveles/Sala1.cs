65296811d76409e84ad95b68743d573988797184
Marco Piatti <marcopiatticastro@hotmail.com>
2022-04-25 10:37:22 -0300

    Agregados escalones en sala 1

--- TGC.MonoGame.TP/Niveles/Sala1.cs
+++ TGC.MonoGame.TP/Niveles/Sala1.cs
@@ -12,19 +12,38 @@ namespace TGC.MonoGame.TP.Niveles
 {
     public class Sala1 : Sala
     {
-        private CylinderPrimitive UnObstaculo { get; set; }
-        private Matrix UnObstaculoWorld { get; set; }
+        private CylinderPrimitive Columna { get; set; }
+        private Matrix ColumnaWorld { get; set; }
+
+        private CubePrimitive Escalon { get; set; }
+        private List<Matrix> EscalonesWorld { get; set; }
+        private Matrix EscalonScale = Matrix.CreateScale(15f, 1f, 20f);
 
         public Sala1(ContentManager content, GraphicsDevice graphicsDevice, Vector3 posicion) : base(content, graphicsDevice, posicion)
         {
-            UnObstaculo = new CylinderPrimitive(graphicsDevice);
-            UnObstaculoWorld = Matrix.CreateScale(50f, 50f, 50f) * Matrix.CreateTranslation(new Vector3(0, 10f, 0) + posicion);
+            Columna = new CylinderPrimitive(graphicsDevice, 1f, 1f, 8);
+            ColumnaWorld = Matrix.CreateScale(50f, 50f, 50f) * Matrix.CreateTranslation(new Vector3(0, 10f, 0) + posicion);
+
+            Escalon = new CubePrimitive(graphicsDevice);
+            EscalonesWorld = new List<Matrix>()
+
+            float angulo = 0f;
+            for(int i = 0; i < 8; i++){
+                Matrix esc = new Matrix();
+                esc = EscalonScale * Matrix.CreateTranslation(new Vector3(0, 0, 25f)) 
+                            * Matrix.CreateRotationY(MathHelper.ToRadians(angulo)) * Matrix.CreateTranslation(position);
+                EscalonesWorld.add(esc);
+                angulo += 45f;
+            }
         }
 
         public override void Draw(GameTime gameTime, Matrix view, Matrix projection)
         {
             base.Draw(gameTime,view,projection);
-            UnObstaculo.Draw(UnObstaculoWorld, view, projection);
+            Columna.Draw(ColumnaWorld, view, projection);
+            foreach(Matrix esc in EscalonesWorld){
+                Escalon.Draw(esc, view, projection);
+            }
         }
     }
 }
