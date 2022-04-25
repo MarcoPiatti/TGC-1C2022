250086b77b5fcdbe9eb3f2892e47129c064aad33
Marco Piatti <marcopiatticastro@hotmail.com>
2022-04-25 11:29:22 -0300

    Objeto Coin para reutilizar

---
+++ TGC.MonoGame.TP/Elements/Coin.cs
@@ -0,0 +1,33 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using TGC.MonoGame.TP.Geometries;
+
+namespace TGC.MonoGame.TP.Elements
+{
+    public class Coin
+    {
+        private SpherePrimitive body { get; set; }
+        private Matrix CoinWorld { get; set; }
+        private Vector3 position { get; set; }
+        private float CoinRotation { get; set; }
+        private float CoinAngle { get; set; }
+
+        public Coin(GraphicsDevice graphicsDevice, Vector3 posicion){
+            body = new SpherePrimitive(graphicsDevice);
+            CoinWorld = Matrix.CreateScale(1f, 10f, 10f) * Matrix.CreateTranslation(posicion);
+            position = posicion;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            CoinAngle += 0.01f;
+            Matrix rotation = Matrix.CreateRotationY(CoinAngle);
+            CoinWorld = Matrix.CreateScale(1f, 10f, 10f) * rotation * Matrix.CreateTranslation(position);
+        }
+
+        public void Draw(Matrix view, Matrix projection){
+            body.Draw(CoinWorld, view, projection);
+        }
+    }
+}
