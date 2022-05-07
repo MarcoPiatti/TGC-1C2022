d49f488a2ca4e4539ce2e4e580f190a33fd4b8d5
Maqueda Paul <maquedapablodavid@gmail.com>
2022-05-07 20:57:07 -0300

    Subo cambios

--- TGC.MonoGame.TP/State.cs
+++ TGC.MonoGame.TP/State.cs
@@ -19,13 +19,27 @@ namespace TGC.MonoGame.TP
     */
     public class Metal : State { }
     /* 
+     Velocidad 90%
+     Rebote = 0,7
+     Salto = 0.8
+     Peso = 2
      
     */
     public class Piedra : State { }
     /* 
-     
+     Velocidad 100%
+     Rebote = 1 (normal)
+     Salto = 1 
+     Peso = 1
     */
     public class Plastico : State { }
+    /* 
+     Velocidad 200%
+     Rebote = 1.5
+     Salto = 1.5
+     Peso =
+     
+    */
 
 
 
